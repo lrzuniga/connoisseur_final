8ae0551e8ab8b974e54887cb6467c6a143833cb2
lrzuniga <lrzuniga@gmail.com>
2013-09-03 17:49:42 -0400

    working version

--- html_generator.rb
+++ html_generator.rb
@@ -9,14 +9,23 @@ class HtmlGenerator
     products = raw_response
     products.each do | product |
       puts "<h2>#{product['name']}</h2>"
+      puts "<img src='#{product['image_thumb_url']}'/>"
       puts "#{product['id']}"
-      puts "<img src='#{product['image_thumb_url']}'</>"
     end
     html_footer
   end
 
 
   def show(id)
+    html_header
+    raw_response = open("http://lcboapi.com/products/#{id}.json").read
+    parse_response = JSON.parse(raw_response)
+    product = parse_response["result"]
+    
+    puts "<h2>#{product['name']}</h2>"
+    puts "<img src='#{product['image_thumb_url']}'/>"
+    puts "id: #{product['id']}"
+    html_footer
   end
 
   def html_header
@@ -44,6 +53,13 @@ class HtmlGenerator
     parse_response = JSON.parse(raw_response)
     products = parse_response["result"]
   end
+
+  def raw_response_id(id)
+    raw_response = open("http://lcboapi.com/products/#{id}.json").read
+    parse_response = JSON.parse(raw_response_id)
+    product = parse_response["result"]
+  end
+
 
 
 end
