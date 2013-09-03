617e563e21f488d2bf093002fafcb0be5079ba56
lrzuniga <lrzuniga@gmail.com>
2013-09-03 18:50:01 -0400

    added css and refactored

--- html_generator.rb
+++ html_generator.rb
@@ -2,65 +2,52 @@ require 'json'
 require 'open-uri'
 
 class HtmlGenerator
+
   def index
     html_header
-    puts "<h1>All Products</h1>"
+      puts "<h1>All Products</h1>"
 
-    products = raw_response
-    products.each do | product |
-      puts "<h2>#{product['name']}</h2>"
-      puts "<img src='#{product['image_thumb_url']}'/>"
-      puts "#{product['id']}"
-    end
+      raw_response = open("http://lcboapi.com/products.json").read
+      parse_response = JSON.parse(raw_response)
+      products = parse_response["result"]
+      
+      products.each do | product |
+        puts "<h2>#{product['name']}</h2>"
+        puts "<img src='#{product['image_thumb_url']}'/>"
+        puts "#{product['id']}"
+      end
     html_footer
   end
 
-
   def show(id)
     html_header
-    raw_response = open("http://lcboapi.com/products/#{id}.json").read
-    parse_response = JSON.parse(raw_response)
-    product = parse_response["result"]
+      raw_response = open("http://lcboapi.com/products/#{id}.json").read
+      parse_response = JSON.parse(raw_response)
+      product = parse_response["result"]
     
-    puts "<h2>#{product['name']}</h2>"
-    puts "<img src='#{product['image_thumb_url']}'/>"
-    puts "id: #{product['id']}"
+      puts "<h2>#{product['name']}</h2>"
+      puts "id: #{product['id']}"
+      puts "<div id='product_image'>"
+      puts "<img src='#{product['image_thumb_url']}'/>"
+      puts "</div>"
+
     html_footer
   end
 
   def html_header
     puts "<!DOCTYPE html>"
     puts "<html>"  
-    puts "  <head>"
-    puts "    <title>BML Connoisseur Assignment</title>"
-    puts "  </head>"
-    puts "  <body>"
+    puts "<head>"
+    puts "<title>BML Connoisseur Assignment</title>"
+    puts "<link rel='stylesheet' type='text/css' href='style.css'> "
+    puts "</head>"
+    puts "<body>"
   end
 
   def html_footer
-    puts "  </body>"
+    puts "</body>"
     puts "</html>"
   end
-
-  def print_products
-  end
-
-  def print_product
-  end
-
-  def raw_response
-    raw_response = open("http://lcboapi.com/products.json").read
-    parse_response = JSON.parse(raw_response)
-    products = parse_response["result"]
-  end
-
-  def raw_response_id(id)
-    raw_response = open("http://lcboapi.com/products/#{id}.json").read
-    parse_response = JSON.parse(raw_response_id)
-    product = parse_response["result"]
-  end
-
-
-
+  
 end
 
